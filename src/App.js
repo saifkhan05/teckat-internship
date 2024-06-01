f454a1a2c6e0032cc43f140d5651d0f6e37b8cd9
saifkhan05 <saifaa7860@gmail.com>
2024-06-01 18:24:13 +0530

    project1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  // let str = "Hello"
+  // let str2 = "world"
+
+  // if(Str === "Hello" && str2 === "world")
+  //   {
+  //     console.log("hello world")
+  //   } 
+
+  const student = {
+    name: "saif",
+    age: 19,
+    address: "mango",
+  
+  }
+    const student2 = {
+
+      name: "saifee",
+      age: 18,
+      address: "jamshedpur",
+  };
+  if (student.name === student2.name) {
+    console.log("hello");
+  } else {
+    console.log("world");
+  }
+  
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
