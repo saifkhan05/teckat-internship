c7538a220b65db5d9b654fd40c01761f20dfc706
saifkhan05 <saifaa7860@gmail.com>
2024-06-06 16:27:33 +0530

    react-with-js created

--- src/App.js
+++ src/App.js
@@ -10,24 +10,56 @@ function App() {
   //     console.log("hello world")
   //   } 
 
-  const student = {
-    name: "saif",
-    age: 19,
-    address: "mango",
+  // const student = {
+  //   name: "saif",
+  //   age: 19,
+  //   address: "mango",
   
-  }
-    const student2 = {
+  // }
+  //   const student2 = {
 
-      name: "saifee",
-      age: 18,
-      address: "jamshedpur",
-  };
-  if (student.name === student2.name) {
-    console.log("hello");
-  } else {
-    console.log("world");
-  }
+  //     name: "saifee",
+  //     age: 18,
+  //     address: "jamshedpur",
+  // };
+  // if (student.name === student2.name) {
+  //   console.log("hello");
+  // } else {
+  //   console.log("world");
+  // }
+
+
+  const people = [ 
+    { name: "saif" age: 33 },
+    { name: "faizan" age: 12 },
+    { name: "kaif", age: 22 },
+  ] ; 
+
+  // for (let item of people) { 
+  //   console.log(item.name)
+  // };
+
+  people.forEach((person) =>
+{
+  // console.log(person.name)
+});
+
+console.log(employee.name);
+const employee = {
+   name: "saif",
+   age: 33,
+   occupation: "engineer",
+};
   
+
+for (const key in employee) {
+   if (employee.hasOwnproperty(key)) {
+    console.log(`${key}: ${employee[key]}`);
+
+  
+   }
+}
+ 
     return (
     <div className="App">
       <header className="App-header">
